f567bed6479d0000afd5be2459af012551d93aa9
vislower <115408772+vislower@users.noreply.github.com>
2023-09-04 15:14:55 +0200

    Add SWAP feature

--- src/main/java/io/vislower/forth/Swap.java
+++ src/main/java/io/vislower/forth/Swap.java
@@ -6,8 +6,11 @@ class Swap implements ForthWord{
 
     @Override
     public void accept(Deque<Integer> stack) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'accept'");
+        ensureProperSize(2, stack.size(), "Swapping requires that the stack contain at least 2 values");
+        int first = stack.pop();
+        int second = stack.pop();
+        stack.push(first);
+        stack.push(second);
     }
     
 }
